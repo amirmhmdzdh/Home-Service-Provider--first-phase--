8780cce4668ffc1b4d362e01d21eab2747b0e2ad
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-06 03:59:57 +0430

    Create OrderStatus

---
+++ src/main/java/org/achareh/model/order/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package org.achareh.model.order.enums;
+
+public enum OrderStatus {
+    WAITING_FOR_SPECIALIST_SUGGESTION,
+    WAITING_FOR_SPECIALIST_SELECTION,
+    WAITING_FOR_SPECIALIST_TO_COME,
+    STARTED,
+    DONE,
+    PAID
+}

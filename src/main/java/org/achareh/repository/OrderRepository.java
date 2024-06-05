85518bbe010fbdf301c56378aa596d3a9662b3b1
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-05 22:51:41 +0430

    Create OrderRepository

---
+++ src/main/java/org/achareh/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package org.achareh.repository;
+
+import org.achareh.base.repository.BaseRepository;
+import org.achareh.model.order.Orders;
+
+
+public interface OrderRepository extends BaseRepository<Orders, Long> {
+}

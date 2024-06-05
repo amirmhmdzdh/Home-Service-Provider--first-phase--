c08d18285bda580874a5d07a2530331d48a622e5
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-05 22:44:12 +0430

    Create MainService.java

---
+++ src/main/java/org/achareh/service/MainServiceService.java
@@ -0,0 +1,7 @@
+package org.achareh.service;
+
+import org.achareh.base.service.BaseService;
+import org.achareh.model.service.MainService;
+
+public interface MainServiceService extends BaseService<MainService,Long> {
+}
